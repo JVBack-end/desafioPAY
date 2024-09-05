df4b06bf4293008c555ae4203fdcb656ebfa7c95
Amanda <amanda.simoes1707@gmail.com>
2024-09-04 22:23:55 -0300

    feat: use case para transferencia bancaria

--- src/main/java/com/example/desafio/app/restcontroller/TransferRestController.java
+++ src/main/java/com/example/desafio/app/restcontroller/TransferRestController.java
@@ -1,15 +1,22 @@
 package com.example.desafio.app.restcontroller;
 
+import com.example.desafio.domain.dto.request.TransferDtoRequest;
+import com.example.desafio.domain.usecase.TransferUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TransferRestController {
 
+    @Autowired
+    private TransferUseCase transferUseCase;
+
     @PostMapping("/transfer")
-    public ResponseEntity transfer() {
-        return ResponseEntity.ok().body("Transfer criado com sucesso!");
+    public ResponseEntity transfer(@RequestBody TransferDtoRequest transferDtoRequest) throws Exception {
+        return ResponseEntity.ok().body(transferUseCase.transfer(transferDtoRequest));
     }
 
 
